17efd00a01568ee55dc632d3e1339b1ebd1e68ac
Zero <valzorian.exe@gmail.com>
2024-07-05 12:57:12 +0300

    Проверка переменных

--- main.cpp
+++ main.cpp
@@ -3,5 +3,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Game over!" << endl;
+    unsigned int score = 5000;
+    cout << "Score: " << score << endl;
+    score = score + 100;
+    cout << "Score: " << score << endl;
+    score += 100;
+    cout << "Score: " << score << endl;
+    int lives = 3;
+    ++lives;
+    cout << "Lives: " << lives << endl;
+    lives = 3;
+    lives++;
+    cout << "Lives: " << lives << endl;
+    lives = 3;
+    int bonus = ++lives * 10;
+    cout << "Lives, bonus = " << lives << ", " << bonus << endl;
+    lives = 3;
+    bonus = lives++ * 10;
+    cout << "Lives, bonus = " << lives << ", " << bonus << endl;
+    score = 4294967295;
+    cout << "\nscore: " << score << endl;
+    ++score;
+    cout << "score: " << score << endl;
+    return 0;
 }
